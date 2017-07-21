154879f246bb60cfe2465e11832cbf14c6735346
j0ntan <jgamboa0209@gmail.com>
2017-07-21 06:24:14 -0400

    Added class for fully functional shift register

---
+++ ShiftRegister74595.cpp
@@ -0,0 +1,47 @@
+#include <ShiftRegister74595.h>
+#include <Arduino.h>
+
+namespace {
+    using ShiftReg595::pin;
+    void
+    setPinModes(const pin& serial, const pin& shiftCLK,
+            const pin& storageCLK, const pin& outEn, const pin& shiftClear) {
+        pinMode(serial, OUTPUT);
+        pinMode(shiftCLK, OUTPUT);
+        pinMode(storageCLK, OUTPUT);
+        pinMode(outEn, OUTPUT);
+        pinMode(shiftClear, OUTPUT);
+    }
+}
+
+namespace ShiftReg595 {
+    ShiftRegister::ShiftRegister(pin serial, pin storageCLK,
+            pin shiftCLK, pin outputEnable, pin serialClear)  : 
+            OutEn_(outputEnable), SRCLR_(serialClear),
+            ShiftRegisterBase(serial, storageCLK, shiftCLK) {
+        setPinModes(SER_, SRCLK_, RCLK_, OutEn_, SRCLR_);
+        outputAllOff();
+        enableOutput();
+    }
+    ShiftRegister::~ShiftRegister() {}
+
+    void
+    ShiftRegister::enableOutput() const {
+        OutEn_.enableOutput();
+    }
+    void
+    ShiftRegister::disableOutput() const {
+        OutEn_.disableOutput();
+    }
+
+    void
+    ShiftRegister::clearShiftRegisters() const {
+        SRCLR_.clearShiftRegisters(period_SRCLK);
+    }
+    void
+    ShiftRegister::outputAllOff() const {
+        // override inherited, faster with SRCLR
+        SRCLR_.clearShiftRegisters(period_SRCLK);
+        ticRCLK();
+    }
+}
--- ShiftRegister74595.h
+++ ShiftRegister74595.h
@@ -5,4 +5,23 @@
 #include <utility/ShiftRegisterEnable.h>
 #include <utility/ShiftRegisterClear.h>
 
+namespace ShiftReg595 {
+    class ShiftRegister : public ShiftRegisterBase{
+    public:
+        ShiftRegister(pin serial, pin storageCLK,
+                pin shiftCLK, pin outputEnable, pin serialClear);
+        virtual ~ShiftRegister();
+
+        void enableOutput() const;
+        void disableOutput() const;
+
+        void clearShiftRegisters() const;
+        void outputAllOff() const;
+
+    private:
+        const OutputEnable OutEn_;
+        const SRClear SRCLR_;
+    };
+}
+
 #endif
